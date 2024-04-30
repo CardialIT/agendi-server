688c9c5155fa7d0f30d1150d73f5bcf308e4dfce
Luan Evander <luanevander.dev@gmail.com>
2024-04-30 20:20:17 -0300

    Registro dos módulos de "type"

---
+++ src/modules/type/type.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeController } from './type.controller';
+import { TypeService } from './type.service';
+import { PrismaService } from 'src/database/PrismaService';
+
+@Module({
+  controllers: [TypeController],
+  providers: [TypeService, PrismaService],
+})
+export class ServiceTypeModule {}
