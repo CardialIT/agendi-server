77ae1af4b5fe5834a0ff25c19d74b7332820f341
Luan Evander <luanevander.dev@gmail.com>
2024-04-30 20:17:05 -0300

    POST para criação de tipo

---
+++ src/modules/type/type.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { TypeService } from './type.service';
+import { Type } from '@prisma/client';
+
+@Controller()
+export class TypeController {
+  constructor(private readonly TypeService: TypeService) {}
+
+  @Post('type')
+  async createServiceType(@Body('name') name: string): Promise<Type> {
+    return this.TypeService.createType(name);
+  }
+}
