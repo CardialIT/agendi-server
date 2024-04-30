f4629b079cc5acbaac2f5f33d1013f015e855471
Luan Evander <luanevander.dev@gmail.com>
2024-04-30 20:24:13 -0300

    Registro do TypeModule em AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@
 import { Module } from '@nestjs/common';
 import { SchedulingModule } from './modules/Scheduling/scheduling.module';
 import { ProfessionalsModule } from './modules/professionals/professionals.module';
+import { TypeModule } from './modules/type/type.module';
 
 @Module({
-  imports: [SchedulingModule, ProfessionalsModule],
+  imports: [SchedulingModule, ProfessionalsModule, TypeModule],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
+  
