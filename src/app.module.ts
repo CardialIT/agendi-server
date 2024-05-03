63cbd422cbc31fc446c3aa4f7ac178a68e20367c
Luan Evander <luanevander.dev@gmail.com>
2024-05-03 18:37:42 -0300

    Registro do ClientModule no gerenciador de módulos da aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { SchedulingModule } from './modules/Scheduling/scheduling.module';
 import { ProfessionalsModule } from './modules/professionals/professionals.module';
 import { TypeModule } from './modules/type/type.module';
+import { ClientModule } from './modules/client/client.module';
 
 @Module({
-  imports: [SchedulingModule, ProfessionalsModule, TypeModule],
+  imports: [SchedulingModule, ProfessionalsModule, TypeModule, ClientModule],
   controllers: [],
   providers: [],
 })
