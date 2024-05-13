4cb565935428b7f4def10cead174aad6c3ac49b4
Luan Evander <luanevander.dev@gmail.com>
2024-05-13 20:21:42 -0300

    Refatoração de atributos do DTO Scheduling

--- src/modules/Scheduling/scheduling.dto.ts
+++ src/modules/Scheduling/scheduling.dto.ts
@@ -1,11 +1,9 @@
 /* eslint-disable */
-import { Professional } from '@prisma/client';
 
 export type SchedulingDTO = {
-  id: string;
-  profissional: string;
-  type: string;
-  client: string;
-  date: string;
-  hour: string;
+  professionalId: number;
+  typeId: number;
+  clientId: number;
+  scheduleDate: Date;
+  schedulingDate: Date;
 };
