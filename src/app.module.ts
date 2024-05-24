2889e207215d3e27efc032eba4972c7f6d7a0139
Luan Evander <luanevander.dev@gmail.com>
2024-05-24 14:24:11 -0300

    Adiciona módulo de usuário ao app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { SchedulingModule } from './modules/Scheduling/scheduling.module';
 import { ProfessionalsModule } from './modules/professionals/professionals.module';
 import { JobTypeModule } from './modules/jobType/jobType.module';
 import { ClientModule } from './modules/client/client.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [SchedulingModule, ProfessionalsModule, JobTypeModule, ClientModule],
+  imports: [SchedulingModule, ProfessionalsModule, JobTypeModule, ClientModule, UserModule],
   controllers: [],
   providers: [],
 })
