336af183484d7aaaafb2aa6bc7280cb376044b78
Luan Evander <luanevander.dev@gmail.com>
2024-05-27 19:21:14 -0300

    Remoção módulo de tipo de trabalho

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@
 import { Module } from '@nestjs/common';
 import { SchedulingModule } from './modules/Scheduling/scheduling.module';
 import { ProfessionalsModule } from './modules/professionals/professionals.module';
-import { JobTypeModule } from './modules/jobType/jobType.module';
 import { ClientModule } from './modules/client/client.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [SchedulingModule, ProfessionalsModule, JobTypeModule, ClientModule, UserModule],
+  imports: [SchedulingModule, ProfessionalsModule, ClientModule, UserModule],
   controllers: [],
   providers: [],
 })
